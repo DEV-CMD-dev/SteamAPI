27ee169a21e1c60f691e757048dd57d0a1b07f4c
Maksym-Rubel <rubelmaksum2404@gmail.com>
2026-07-01 10:34:58 +0300

    Update enum for user entity

--- DataAccess/Data/Entities/User.cs
+++ DataAccess/Data/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using DataAccess.Enums;
+using Microsoft.AspNetCore.Identity;
 
 namespace DataAccess.Data.Entities
 {
@@ -9,8 +10,8 @@ namespace DataAccess.Data.Entities
         public string? Country { get; set; }
         public DateOnly CreatedAt { get; set; }
         public DateTime LastOnline { get; set; }
-        //public UserVisibility UserVisibility { get; set; }
-        //public UserRole UserRole { get; set; }
+        public UserVisibility UserVisibility { get; set; }
+        public UserRole UserRole { get; set; }
         public decimal WalletBalance { get; set; }
 
         public List<UserGame> OwnedGames { get; set; } = new();
--- DataAccess/Enums/UserVisibility.cs
+++ DataAccess/Enums/UserVisibility.cs
@@ -1,4 +1,4 @@
-﻿namespace BusinessLogic.Enums
+﻿namespace DataAccess.Enums
 {
     public enum UserVisibility
     {
