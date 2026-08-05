bece5727dfffc694bc2e584802f9edc3ad37006c
Anton Yurkovets <overgeardev@gmail.com>
2026-08-06 00:32:48 +0300

    Remove automapping for Patch in Game/Tag services Add missing validations in Game DTOs

--- BusinessLogic/DTOs/Game/PutGameDto.cs
+++ BusinessLogic/DTOs/Game/PutGameDto.cs
@@ -1,10 +1,13 @@
-﻿namespace BusinessLogic.DTOs.Game
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLogic.DTOs.Game
 {
     public class PutGameDto
     {
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime? ReleaseDate { get; set; }
+        [Range(0, double.MaxValue)]
         public decimal Price { get; set; }
         public int Discount { get; set; }
         public string SystemRequirements { get; set; }
