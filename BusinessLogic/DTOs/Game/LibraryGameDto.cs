01ee57f8de85c6059239b055094412181834efdd
Maksym-Rubel <rubelmaksum2404@gmail.com>
2026-09-17 13:55:53 +0300

    add library service

---
+++ BusinessLogic/DTOs/Game/LibraryGameDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.DTOs.Game
+{
+    public class LibraryGameDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string IconUrl { get; set; } = string.Empty;
+    }
+}
---
+++ BusinessLogic/Interfaces/ILibraryService.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLogic.DTOs.Game;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface ILibraryService
+    {
+        Task<List<LibraryGameDto>> GetUserGames(string userId);
+    }
+}
